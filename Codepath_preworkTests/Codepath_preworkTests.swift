d5910a4244383c8bb5c48836871d4be4f3b72f9c
Owner <owner@Owners-MacBook-Pro.local>
2025-01-25 01:50:39 -0500

    Initial commit

---
+++ Codepath_preworkTests/Codepath_preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Codepath_preworkTests.swift
+//  Codepath_preworkTests
+//
+//  Created by Owner on 1/25/25.
+//
+
+import Testing
+@testable import Codepath_prework
+
+struct Codepath_preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
